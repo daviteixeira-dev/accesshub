e7e7400dc3258156bb53b669a717cd0625c994cf
Davi Teixeira <daviteixeira.btm@gmail.com>
2022-10-30 23:16:20 -0300

    Project Components styling

--- web/src/pages/Components.tsx
+++ web/src/pages/Components.tsx
@@ -1,17 +1,108 @@
-import { 
-  Flex, 
-  Text, 
-  Container, 
+import {
+  Flex,
+  Text,
+  Heading,
+  Container,
 } from '@chakra-ui/react'
 
 import MenuSideBar from '../components/MenuSideBar/MenuSideBar'
 
+import ComponentCardItem from '../components/ComponentCardItem/ComponentCardItem'
+
 const Components = () => {
   return (
-    <Flex>
+    <Flex bg='blackAlpha.50'>
+      
       <MenuSideBar />
-      <Container maxW='container.xl'>
-        <Text>Componentes</Text>
+      
+      <Container maxW='container.xl' height='100vh' overflowY='auto'>
+
+        <Heading 
+          as='h1' 
+          my='1rem'
+          fontSize='2rem' 
+          color='gray.900' 
+        >
+          Componentes
+        </Heading>
+
+        <Text 
+          my='2rem' 
+          fontSize='1rem' 
+          fontWeight='normal'
+        >
+          O AcessHub fornece um guia de implementação dos componentes para ajudá-lo a construir 
+          seus projetos mais rapidamente. Aqui está uma visão geral das categorias de componentes:
+        </Text>
+
+        <Heading 
+          my='2rem'
+          fontSize='1.5rem' 
+        >
+          Exemplo de botões
+        </Heading>
+
+        <Flex justifyContent='space-evenly' mb='2rem'>
+          <ComponentCardItem 
+            cardTitle='Botão' 
+            pagLink='/button' 
+            altImage='teste 1'
+            imgLink='https://via.placeholder.com/150' 
+          />
+          <ComponentCardItem 
+            cardTitle='Botão' 
+            pagLink='/button' 
+            altImage='teste 2'
+            imgLink='https://via.placeholder.com/150' 
+          />
+          <ComponentCardItem 
+            cardTitle='Botão' 
+            pagLink='/button' 
+            altImage='teste 3'
+            imgLink='https://via.placeholder.com/150' 
+          />
+          <ComponentCardItem 
+            cardTitle='Botão' 
+            pagLink='/button' 
+            altImage='teste 4'
+            imgLink='https://via.placeholder.com/150' 
+          />
+        </Flex>
+
+        <Heading 
+          my='2rem'
+          fontSize='1.5rem' 
+        >
+          Exemplo de botões
+        </Heading>
+        
+        <Flex justifyContent='space-evenly' mb='2rem'>
+          <ComponentCardItem 
+            cardTitle='Botão' 
+            pagLink='/button' 
+            altImage='teste 1'
+            imgLink='https://via.placeholder.com/150' 
+          />
+          <ComponentCardItem 
+            cardTitle='Botão' 
+            pagLink='/button' 
+            altImage='teste 2'
+            imgLink='https://via.placeholder.com/150' 
+          />
+          <ComponentCardItem 
+            cardTitle='Botão' 
+            pagLink='/button' 
+            altImage='teste 3'
+            imgLink='https://via.placeholder.com/150' 
+          />
+          <ComponentCardItem 
+            cardTitle='Botão' 
+            pagLink='/button' 
+            altImage='teste 4'
+            imgLink='https://via.placeholder.com/150' 
+          />
+        </Flex>
+
       </Container>
     </Flex>
   )
