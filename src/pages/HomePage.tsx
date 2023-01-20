2a5576c094d16bc0e4400f10a915557c6f5d1c08
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-01-19 22:49:24 -0300

    refactoring and adding the project goals section to the project home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,17 +3,22 @@ import {
   Text,
   Link,
   Icon,
-  Heading
-} from '@chakra-ui/react'
+  Card,
+  Stack,
+  Avatar,
+  Heading,
+  CardBody
+} from '@chakra-ui/react';
 
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
-import { FaGithub, FaArrowRight } from 'react-icons/fa'
-import TopMenu from '../components/TopMenu/TopMenu'
-import Footer from '../components/Footer/Footer'
+import Footer from '../components/Footer/Footer';
+import TopMenu from '../components/TopMenu/TopMenu';
+
+import { FaGithub, FaArrowRight } from 'react-icons/fa';
 
 const HomePage = () => {
-
+  
   return (
     <Flex
       width='100%'
@@ -22,30 +27,31 @@ const HomePage = () => {
       alignItems='center'
       flexDirection='column'
       justifyContent='center'
-      height={['100%', '100%', '100vh', '100vh']}
+      height={['100%', '100%', '100%', '100%']}
     >
       <TopMenu />
 
       <Flex
+        mb='1rem'
         height='100%'
         padding='1rem'
         flexDirection='column'
         pt={['6rem', 0, 0, 0 ]}
         mt={[0, 0, '6rem', '6rem']}
-        width={['100%', '100%', '50%', '50%']}
+        width={['100%', '100%', '60%', '60%']}
       >
         <Heading
           as='h1'
           color='gray.900'
           textTransform='uppercase'
-          fontSize={['2.5rem', '2.5rem', '3rem', '3rem']}
+          fontSize={['2.5rem', '2.5rem', '3rem', '3.5rem']}
         >
           Desenvolva e deixe o seu site mais <Text as='span' color='blue.500'>acessível</Text>
         </Heading>
         <Text
           color='gray.800'
           marginTop='1rem'
-          fontSize='1.1rem'
+          fontSize='1.5rem'
         >
           AccessHub é um guia sobre funcionalidades de acessibilidade para desenvolvedores Web,
           possibilitando a apresentação de padrões e materiais de suporte para ajudar na
@@ -82,7 +88,8 @@ const HomePage = () => {
 
       <Flex
         mb='1rem'
-        display='flex'        
+        display='flex'
+        alignItems='center'        
         justifyContent='space-evenly'
         width={['100%', '100%', '50%', '40%']}
         flexDirection={['column', 'column', 'row', 'row']}
@@ -133,10 +140,82 @@ const HomePage = () => {
         </Link>
 
       </Flex>
+
+      <Flex
+        mt='2rem'
+        width='100%'
+        bg='gray.200'
+        height='100%'
+        padding='1rem'
+        alignItems='center'
+        flexDirection='column'
+      >
+        <Flex 
+          width={['100%', '100%', '60%', '60%']}
+        >
+          <Heading
+            as='h1'
+            color='gray.900'
+            fontSize={['1.5rem', '1.5rem', '1.8rem', '1.8rem']}
+          >
+            Nosso objetivo, é fornecer exemplos de implementações, 
+            auxiliando no processo de programação de 
+            <Text as='span' color='blue.500'>funcionalidades</Text> de acessibilidade 
+          </Heading>
+        </Flex>
+
+        <Flex 
+          w='100%' 
+          mt='1rem' 
+          justifyContent='space-evenly'
+          flexDirection={['column', 'column', 'row', 'row']}
+        >
+          <Card maxW='sm' mt='1rem'>
+            <CardBody>
+              <Avatar name='Segun Adebayo' src='https://via.placeholder.com/50' />
+              <Stack mt='6' spacing='3'>
+                <Heading size='md'>Padrões de Referências</Heading>
+                <Text>
+                  Desenvolver material de referência para o suporte na implementação das 
+                  funcionalidades de acessibilidade Web por desenvolvedores.
+                </Text>
+              </Stack>
+            </CardBody>
+          </Card>
+
+          <Card maxW='sm' mt='1rem'>
+            <CardBody>
+              <Avatar name='Segun Adebayo' src='https://via.placeholder.com/50' />
+              <Stack mt='6' spacing='3'>
+                <Heading size='md'>Conjunto de Recomendações</Heading>
+                <Text>
+                  Sugerir um conjunto de recomendações para apoiar a implementação das 
+                  funcionalidades de acessibilidade Web.
+                </Text>
+              </Stack>
+            </CardBody>
+          </Card>
+
+          <Card maxW='sm' mt='1rem'>
+            <CardBody>
+              <Avatar name='Segun Adebayo' src='https://via.placeholder.com/50' />
+              <Stack mt='6' spacing='3'>
+                <Heading size='md'>Exemplos Open Source</Heading>
+                <Text>
+                  Disponibilizar exemplos de código open source para apoiar a 
+                  implementação das funcionalidades de acessibilidade Web.
+                </Text>
+              </Stack>
+            </CardBody>
+          </Card>
+          
+        </Flex>
       
+      </Flex>
+
       <Footer />
     </Flex>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
