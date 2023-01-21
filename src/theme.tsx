379d9458e3c46d2733849509f2b170290a414a8e
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-01-21 18:08:40 -0300

    removing unused configuration from theme file

--- src/theme.tsx
+++ src/theme.tsx
@@ -4,22 +4,7 @@ const theme = {
     config: {
         intialColorMode: "light",
         useSystemColorMode: true,
-    },
-    styles: {
-        global: {
-            body: {
-                margin: 0,
-                "font-family":
-                "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen','Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',sans-serif",
-                "-webkit-font-smoothing": "antialiased",
-                "-moz-osx-font-smoothing": "grayscale",
-            },
-
-            code: {
-                "font-family": "source-code-pro, Menlo, Monaco, Consolas, 'Courier New',monospace",
-            },
-        }
     }
 };
 
-export default extendTheme(theme)
+export default extendTheme(theme);
