ba1a594805ee1981556ee2a0d2a66239b888bb90
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-01-29 22:50:39 -0300

    more content for keyboard navigation page

--- src/pages/PageAboutKeyboardNavigation.tsx
+++ src/pages/PageAboutKeyboardNavigation.tsx
@@ -97,14 +97,12 @@ const PageAboutKeyboardNavigation = () => {
                         </Table>
                     </TableContainer>
 
-                    <Text my='1rem' fontWeight='bold'>
+                    <Text mb='1rem' fontWeight='bold'>
                         OBS: * Alguns manipuladores de eventos são dispositivos-independentes, ou 
                         seja, se aplicam a qualquer dispositivo (mouse, teclado ou outro), como é 
                         o caso de: onFocus, onBlur, onSelect, onChange, e onClick (quando o 
                         onClick for utilizado em um link ou elemento de formulário).
                     </Text>
-
-                    <PostComponent article_name={''} />
 
                     <Text mb='1rem'>
                         Quando forem utilizados múltiplos manipuladores de eventos para uma ação, de 
@@ -134,7 +132,41 @@ const PageAboutKeyboardNavigation = () => {
                         devem ser acessíveis pelo teclado. 
                     </Text>
 
+                    <Heading as='h3' mb='1rem' fontSize='1rem'>
+                        Exemplo
+                    </Heading>
 
+                    <Text mb='1rem'>
+                        Um exemplo de funcionalidade de um site em javascript seria uma página com uma 
+                        tabela de dados. A tabela contém cabeçalhos de coluna e linha, e cada célula 
+                        contém um valor. A funcionalidade permite que o usuário navegue pela tabela 
+                        usando apenas o teclado, sem precisar usar o mouse.
+                    </Text>
+
+                    <Text mb='1rem'>
+                        A funcionalidade é implementada usando o evento "tabindex" do javascript. Quando 
+                        o usuário pressiona a tecla Tab, o foco muda para a próxima célula na 
+                        tabela. Quando o usuário pressiona as setas do teclado, o foco muda para a 
+                        célula adjacente na direção da seta pressionada. O usuário também pode usar as 
+                        teclas Enter ou Space para selecionar uma célula específica.
+                    </Text>
+
+                    <Text mb='1rem'>
+                        Além disso, a funcionalidade fornece mecanismos de navegação de teclado para 
+                        acessar o cabeçalho de coluna e linha corrente, permitindo que o usuário 
+                        entenda qual é o contexto dos dados que estão sendo exibidos.
+                    </Text>
+
+                    <PostComponent article_name={''} />
+
+                    <Text my='1rem'>
+                        Essa funcionalidade é importante porque permite que pessoas com dificuldades 
+                        de movimento, como pessoas com problemas de mobilidade física, possam acessar 
+                        e interagir com o conteúdo da tabela de forma eficiente e independente. Além 
+                        disso, ela também beneficia usuários que preferem usar o teclado em vez do 
+                        mouse, como pessoas com dificuldades de visão, que podem encontrar dificuldades 
+                        para rastrear o cursor do mouse na tela.
+                    </Text>
 
                 </Flex>
                 
