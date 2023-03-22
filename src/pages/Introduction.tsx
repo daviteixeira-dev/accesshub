a64305b9288c190fef1aafe79d0ff0ef1ed49add
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-22 17:27:47 -0300

    adding the font family Inter to the project and refactoring the textual components of the Introduction page

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -22,19 +22,38 @@ const Introduction = () => {
         <MenuSideBar />
       </Hide>
 
-      <Container maxW='container.xl' height='100vh' overflowY='auto' p='0'>
+      <Container
+        p='0'
+        height='100vh'
+        overflowY='auto'
+        maxW='container.xl'
+      >
         
         <Show below='md'>
           <MenuMobile marginBottom='0'/>
         </Show>
 
-        <Flex p='1rem' flexDirection='column'>
+        <Flex
+          p='1rem'
+          flexDirection='column'
+        >
 
-          <Heading as='h1' my='1rem' fontSize='2rem'>
+          <Heading
+            as='h1'
+            my='1rem'
+            fontSize='2rem'
+            fontWeight='700'
+            fontFamily='Inter'
+          >
             Sobre o AccessHub
           </Heading>
 
-          <Text mb='1rem'>
+          <Text
+            mb='1rem'
+            fontWeight='400'
+            fontSize='1.2rem'
+            fontFamily='Inter'
+          >
             É importante garantir o desenvolvimento de conteúdos ou serviços acessíveis na internet 
             que estejam disponíveis para todas as pessoas. Para isso, a adoção de práticas de 
             acessibilidade na Web visa tornar os sites mais acessíveis e utilizáveis pelo maior 
@@ -42,19 +61,34 @@ const Introduction = () => {
             características técnicas.
           </Text>
 
-          <Text mb='1rem'>
+          <Text
+            mb='1rem'
+            fontWeight='400'
+            fontSize='1.2rem'
+            fontFamily='Inter'
+          >
             Portanto, implementar funcionalidades de acessibilidade em projetos de desenvolvimento de 
             sistemas Web, significa que pessoas com algum tipo de deficiência podem fazer uso da Web 
             nas mesmas condições que o restante das pessoas. Aplicar as práticas de acessibilidade 
             permite que os usuários possam perceber, compreender, navegar e interagir com a Web.
           </Text>
 
-          <Text mb='1rem'>
+          <Text
+            mb='1rem'
+            fontWeight='400'
+            fontSize='1.2rem'
+            fontFamily='Inter'
+          >
             Entretanto, o desenvolvimento de sistemas Web é, geralmente, realizado sem oferecer o suporte 
             necessário para que pessoas com limitações possam acessá-los sem dificuldades. 
           </Text>
 
-          <Text mb='1rem'>
+          <Text
+            mb='1rem'
+            fontWeight='400'
+            fontSize='1.2rem'
+            fontFamily='Inter'
+          >
             Dessa forma, criamos o AccessHub, um projeto open-source alinhado as diretrizes e recomendações 
             do WCAG e do eMAG que busca oferecer exemplos de implementações como referência, 
             principalmente, para desenvolvedores inexperientes, facilitando no processo de programação de 
@@ -62,49 +96,99 @@ const Introduction = () => {
             podendo ser reutilizados, diminuindo o retrabalho e agilizando o processo de desenvolvimento.
           </Text>
 
-          <Heading as='h2' mb='1rem' fontSize='1.5rem'>
+          <Heading
+            as='h2'
+            mb='1rem'
+            fontWeight='600'
+            fontSize='1.5rem'
+            fontFamily='Inter'
+          >
             Objetivos
           </Heading>
 
-          <Text mb='1rem'>
+          <Text
+            mb='1rem'
+            fontWeight='400'
+            fontSize='1.2rem'
+            fontFamily='Inter'
+          >
             Assim, temos como objetivo lhe proporcionar um guia sobre funcionalidades de acessibilidade para desenvolvedores Web, 
             a fim de prover:
           </Text>
 
           <UnorderedList>
-            <ListItem>
+            <ListItem
+              fontSize='1rem'
+              fontWeight='500'
+              fontFamily='Inter'
+            >
               Apresentação de padrões e materiais de referência para ajudar na construção de sistemas 
               Web acessíveis. 
             </ListItem>
-            <ListItem>
+            <ListItem
+              fontSize='1rem'
+              fontWeight='500'
+              fontFamily='Inter'
+            >
               Sugerir um conjunto de recomendações de funcionalidades para acessibilidade Web.
             </ListItem>
-            <ListItem>
+            <ListItem
+              fontSize='1rem'
+              fontWeight='500'
+              fontFamily='Inter'
+            >
               Fornecer exemplos open source para acessibilidade Web que possam ser facilmente acessados, 
               compreendidos e compartilhados.
             </ListItem>
           </UnorderedList>
 
-          <Heading as='h2' my='1rem' fontSize='1.5rem'>
+          <Heading
+            as='h3'
+            my='1rem'
+            fontWeight='600'
+            fontSize='1.5rem'
+            fontFamily='Inter'
+          >
             Como contribuir?
           </Heading>
 
-          <Text mb='1rem'>
+          <Text
+            mb='1rem'
+            fontWeight='400'
+            fontSize='1.2rem'
+            fontFamily='Inter'
+          >
             O desenvolvimento do AccessHub é uma iniciativa para a construção de um projeto open-sourcer 
             para a comunidade e contribuições são bem vindas. Para contribuir, <Link href='https://github.com/daviteixeira-btm/accesshub'>acesse e use o mecanismo de 
             contribuição do Github</Link>, você pode encontar o passo a passo disponibilizado na descrição do projeto.
           </Text>
 
-          <Heading as='h3' mb='1rem' fontSize='1.5rem'>
+          <Heading
+            as='h4'
+            mb='1rem'
+            fontWeight='600'
+            fontSize='1.5rem'
+            fontFamily='Inter'
+          >
             Referências
           </Heading>
 
-          <Text mb='1rem'>
+          <Text
+            mb='1rem'
+            fontWeight='400'
+            fontSize='1.2rem'
+            fontFamily='Inter'
+          >
             O AccessHub ainda está em desenvolvimento, podendo receber incrementos conforme a pesquisa 
             avança. Até o momento temos o trabalho do qual o projeto está sendo realizado:
           </Text>
 
-          <Text mb='2rem'>
+          <Text
+            mb='1rem'
+            fontWeight='400'
+            fontSize='1.2rem'
+            fontFamily='Inter'
+          >
             SILVA, Davi Teixeira; BEZERRA, Carla Ilane Moreira; SOUZA FILHO, José Cezar 
             Junior de. <Text as='b'>ACCESSHUB: GUIA DE ACESSIBILIDADE PARA DESENVOLVEDORES WEB</Text>. 
             2022. 55 f. TCC (Graduação) - Curso de Sistemas de Informação, Universidade Federal do 
