951d04bda713fbc6f3a39ae46da723c3d174c6d7
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-23 00:46:40 -0300

    refactoring the textual components and adding the Inter font family to the PageAboutListenImageDescription component

--- src/pages/PageAboutListenImageDescription.tsx
+++ src/pages/PageAboutListenImageDescription.tsx
@@ -20,23 +20,48 @@ const PageAboutListenImageDescription = () => {
                 <MenuSideBar />
             </Hide>
 
-            <Container maxW='container.xl' height='100vh' overflowY='auto' p='0'>
+            <Container
+                p='0'
+                height='100vh'
+                overflowY='auto'
+                maxW='container.xl'
+            >
 
                 <Show below='md'>
                     <MenuMobile marginBottom='0'/>
                 </Show>
 
-                <Flex flexDirection='column' p='1rem'>
+                <Flex
+                    p='1rem'
+                    flexDirection='column'
+                >
 
-                    <Heading as='h1' my='1rem'>
+                    <Heading
+                        as='h1'
+                        my='1rem'
+                        fontSize='2rem'
+                        fontWeight='700'
+                        fontFamily='Inter'
+                    >
                         Funcionalidade para ouvir a descrição de uma imagem
                     </Heading>
 
-                    <Heading as='h2' mb='1rem' fontSize='1.1rem'>
+                    <Heading
+                        as='h2'
+                        mb='1rem'
+                        fontSize='1rem'
+                        fontWeight='400'
+                        fontFamily='Inter'
+                    >
                         Ver WCAG 2.0 Critério de Sucesso 1.3.3
                     </Heading>
 
-                    <Text mb='1rem'>
+                    <Text
+                        mb='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         O critério 1.3.3 da WCAG (Web Content Accessibility Guidelines) é conhecido 
                         como "Características sensoriais" e tem como objetivo garantir que as 
                         informações e interações do site possam ser compreendidas e operadas 
@@ -44,7 +69,12 @@ const PageAboutListenImageDescription = () => {
                         exemplo, usuários com deficiência visual, auditiva, motora e cognitiva.
                     </Text>
 
-                    <Text mb='1rem'>
+                    <Text
+                        mb='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         Uma funcionalidade que obedece o critério 1.3.3 - Características sensoriais 
                         da WCAG é a inclusão de descrições de áudio para imagens e elementos gráficos. 
                         Por exemplo, um código javascript que adiciona descrições de áudio para 
@@ -53,7 +83,12 @@ const PageAboutListenImageDescription = () => {
 
                     <PostComponent article_name={'listen-image-description_01'} />
 
-                    <Text my='1rem'>
+                    <Text
+                        my='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         Neste exemplo, depois de colocar uma imagem de sua escolha, quando o cursor do 
                         mouse passa sobre uma imagem, é verificado se a imagem possui uma descrição de 
                         áudio na tag "alt" e, se sim, a função "speak()" é executada para reproduzir 
@@ -61,7 +96,12 @@ const PageAboutListenImageDescription = () => {
                         conteúdo das imagens e melhora a acessibilidade do site.
                     </Text>
 
-                    <Text mb='1rem'>
+                    <Text
+                        mb='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         Para um melhor controle para usuarios que usam o teclado, podemos adicionar um 
                         botão de reprodução HTML e dar-lhe um ID para que possa selecioná-lo no 
                         JavaScript. Em seguida, escrevemos uma função que execute quando o botão é 
@@ -69,7 +109,12 @@ const PageAboutListenImageDescription = () => {
                         ler a descrição da imagem para o usuário.
                     </Text>
 
-                    <Text mb='1rem'>
+                    <Text
+                        mb='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         Ao clicar no botão "Play", a descrição da imagem será lida para o usuário 
                         através da síntese de fala. Isso atenderia ao critério 1.3.3 - Características 
                         sensoriais da WCAG, pois fornece uma alternativa de áudio para o conteúdo 
