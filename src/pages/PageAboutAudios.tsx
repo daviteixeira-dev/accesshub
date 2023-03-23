59ca26b23ee7bb6149a97edb4d05f8a5dec0091d
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-22 23:01:12 -0300

    refactoring the textual components and adding the Inter font family to the PageAboutAudios component

--- src/pages/PageAboutAudios.tsx
+++ src/pages/PageAboutAudios.tsx
@@ -19,27 +19,58 @@ const PageAboutAudios = () => {
                 <MenuSideBar />
             </Hide>
 
-            <Container maxW='container.xl' height='100vh' overflowY='auto' p='0'>
+            <Container
+                p='0'
+                height='100vh'
+                overflowY='auto'
+                maxW='container.xl'
+            >
 
                 <Show below='md'>
                     <MenuMobile marginBottom='0'/>
                 </Show>
 
-                <Flex flexDirection='column' p='1rem'>
+                <Flex
+                    p='1rem'
+                    flexDirection='column'
+                >
 
-                    <Heading as='h1' my='1rem'>
+                    <Heading
+                        as='h1'
+                        my='1rem'
+                        fontSize='2rem'
+                        fontWeight='700'
+                        fontFamily='Inter'
+                    >
                         Áudios
                     </Heading>
 
-                    <Heading as='h2' mb='1rem' fontSize='1.1rem'>
+                    <Heading
+                        as='h2'
+                        mb='1rem'
+                        fontSize='1rem'
+                        fontWeight='400'
+                        fontFamily='Inter'
+                    >
                         Ver WCAG 2.0 Critério de Sucesso 1.2.1, 1.2.2, 1.2.6 e 1.4.2.
                     </Heading>
 
-                    <Heading as='h3' mb='1rem' fontSize='1rem'>
+                    <Heading
+                        as='h3'
+                        mb='1rem'
+                        fontWeight='600'
+                        fontSize='1.5rem'
+                        fontFamily='Inter'
+                    >
                         Fornecer alternativa para áudio (1.2.1, 1.2.2 e 1.2.6)
                     </Heading>
 
-                    <Text mb='1rem'>
+                    <Text
+                        mb='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         Áudio gravado deve possuir uma transcrição descritiva. Além de essencial para 
                         pessoas com deficiência auditiva, a alternativa em texto também é importante 
                         para usuários que não possuem equipamento de som, que desejam apenas realizar a 
@@ -49,16 +80,32 @@ const PageAboutAudios = () => {
 
                     <PostComponent article_name={'audios_01'} />
 
-                    <Text my='1rem'>
+                    <Text
+                        my='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         Uma apresentação prévia do conteúdo dos dois tipos de arquivo e de sua duração 
                         também é desejável.
                     </Text>
 
-                    <Heading as='h3' mb='1rem' fontSize='1rem'>
+                    <Heading
+                        as='h3'
+                        mb='1rem'
+                        fontWeight='600'
+                        fontSize='1.5rem'
+                        fontFamily='Inter'
+                    >
                         Fornecer controle de áudio para som (1.4.2)
                     </Heading>
 
-                    <Text mb='1rem'>
+                    <Text
+                        mb='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         Deve ser fornecido um mecanismo para parar, pausar, silenciar ou ajustar o volume 
                         de qualquer som que se reproduza na página.
                     </Text>
