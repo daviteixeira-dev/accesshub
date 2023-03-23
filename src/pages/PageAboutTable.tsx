17baa7ad9a0cd55029cc3354b8001cc0b34cfda8
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-22 21:36:35 -0300

    refactoring the textual components and adding the font family Inter to the PageAboutTable component

--- src/pages/PageAboutTable.tsx
+++ src/pages/PageAboutTable.tsx
@@ -19,23 +19,48 @@ const PageAboutTable = () => {
                 <MenuSideBar />
             </Hide>
 
-            <Container maxW='container.xl' height='100vh' overflowY='auto' p='0'>
+            <Container
+                p='0'
+                height='100vh'
+                overflowY='auto'
+                maxW='container.xl'
+            >
 
                 <Show below='md'>
                     <MenuMobile marginBottom='0'/>
                 </Show>
 
-                <Flex flexDirection='column' p='1rem'>
+                <Flex
+                    p='1rem'
+                    flexDirection='column'
+                >
 
-                    <Heading as='h1' my='1rem'>
+                    <Heading
+                        as='h1'
+                        my='1rem'
+                        fontSize='2rem'
+                        fontWeight='700'
+                        fontFamily='Inter'
+                    >
                         Tabela
                     </Heading>
 
-                    <Heading as='h2' mb='1rem' fontSize='1.1rem'>
+                    <Heading
+                        as='h2'
+                        mb='1rem'
+                        fontSize='1rem'
+                        fontWeight='400'
+                        fontFamily='Inter'
+                    >
                         Ver WCAG 2.0 Critério de Sucesso 1.3.1 (Técnicas H43 e H63)
                     </Heading>
 
-                    <Text mb='1rem'>
+                    <Text
+                        mb='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         Em tabelas de dados simples, o uso apropriado do elemento TH para os cabeçalhos e do 
                         elemento TD para as células de dados é essencial para torná-las acessíveis. Para 
                         incrementar a acessibilidade, deve-se utilizar os elementos THEAD, TBODY e TFOOT, 
@@ -43,24 +68,45 @@ const PageAboutTable = () => {
                         com exceção de quando a tabela possuir apenas o corpo, sem ter seções de cabeçalho e rodapé.
                     </Text>
 
-                    <Text mb='1rem'>
+                    <Text
+                        mb='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         O W3C sugere utilizar o TFOOT antes do TBODY dentro da definição TABLE para que o agente 
                         de usuário possa renderizar o rodapé antes de receber todas (potencialmente numerosas) 
                         linha de dados. 
                     </Text>
 
-                    <Heading as='h3' mb='1rem' fontSize='1rem'>
+                    <Heading
+                        as='h3'
+                        mb='1rem'
+                        fontWeight='600'
+                        fontSize='1.5rem'
+                        fontFamily='Inter'
+                    >
                         Em tabelas, utilizar títulos e resumos de forma apropriada (1.3.1)
                     </Heading>
 
-                    <Text mb='1rem'>
+                    <Text
+                        mb='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         O título da tabela deve ser definido pelo elemento CAPTION e deve ser o primeiro 
                         elemento utilizado após a declaração do elemento TABLE. Em casos de tabelas extensas, 
                         deve ser fornecido um resumo de seus dados através do atributo summary que deve ser 
                         declarado no elemento TABLE.
                     </Text>
 
-                    <Text mb='1rem'>
+                    <Text
+                        mb='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         Abaixo, um exemplo da implementação de uma Tabela em HTML5 e em seguida sua estlização 
                         em CSS3, sendo esta usada em todos os exemplos de tabela:
                     </Text>
@@ -69,33 +115,58 @@ const PageAboutTable = () => {
 
                     <PostComponent article_name={'table_02'} />
 
-                    <Text mb='1rem'>
+                    <Text
+                        my='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         Para tabelas mais complexas, é necessário utilizar marcações para associar as células de 
                         dados com as células de cabeçalho. A maneira mais adequada de realizar esse procedimento 
                         é utilizar os atributos id/headers ou scope/col.
                     </Text>
 
-                    <Text mb='1rem'>
+                    <Text
+                        mb='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         No primeiro, pode-se associar qualquer célula de conteúdo a qualquer célula de cabeçalho, 
                         utilizando o mesmo valor para o atributo id e para o header. Veja o exemplo abaixo: 
                     </Text>
 
                     <PostComponent article_name={'table_03'} />
 
-                    <Text mb='1rem'>
+                    <Text
+                        my='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         No segundo caso, a associação é automática, sendo mais utilizado em tabelas de associação 
                         direta, nas quais é dado o valor col para o atributo scope nos cabeçalhos. Veja o exemplo abaixo: 
                     </Text>
 
                     <PostComponent article_name={'table_04'} />
 
-                    <Text mb='1rem'>
+                    <Text
+                        my='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         Por fim, um exemplo de tabela com o uso do atributo axis:
                     </Text>
 
                     <PostComponent article_name={'table_05'} />
 
-                    <Text mb='1rem'>
+                    <Text
+                        my='1rem'
+                        fontWeight='400'
+                        fontSize='1.2rem'
+                        fontFamily='Inter'
+                    >
                         Por fim, as tabelas devem ser utilizadas apenas para dados tabulares e não para 
                         efeitos de disposição dos elementos na página. Para este fim, utilize as folhas 
                         de estilo.
